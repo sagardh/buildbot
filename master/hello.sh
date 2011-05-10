619719caa9a318480397f1193ec8a28f37f42755
sagardh <sagardh@gmail.com>
2011-05-10 17:10:04 +0545

    hello.sh modified for test

--- master/hello.sh
+++ master/hello.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 echo "hello world"
-echo "hello immune"
-echo "hello Makalu"
-echo "hello testing"
-echo "hello making"
-echo "hello giving"
-echo "hello passing"
-echo "hello tussing"
-echo "hello Ring"
 echo "hello Ringer"
