a2662016ffae444573ae82e2d607a90259c520c1
Sagar Dhakal <sagardh@gmail.com>
2011-07-19 20:12:19 -0400

    buildbot

--- master/hello.sh
+++ master/hello.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 echo "hello   world"
 echo "hello Pinger"
-echo "hello Tinger"
-echo "hello B   elle Belle"
-echo "dalle dalle"
-echo "Challe Challe    "
 echo "TALLE       "
 echo "SFDf       "
